ab6b9179149b441b497bb5665937fb1d6f306444
Mostafa <Mostafa.Ashraf-Shaaban@vodafone.com>
2019-10-02 01:23:04 +0200

    Format view model

--- app/src/main/java/com/revolut/currency/ui/rates/viewmodel/CurrencyViewModel.kt
+++ app/src/main/java/com/revolut/currency/ui/rates/viewmodel/CurrencyViewModel.kt
@@ -1,33 +1,28 @@
 package com.revolut.currency.ui.rates.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.*
 import com.revolut.currency.networking.RatesService
 import com.revolut.currency.ui.rates.model.RatesMappedModel
 import com.revolut.currency.ui.rates.model.RatesResponse
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.koin.core.KoinComponent
-import java.lang.Exception
 
 
 class CurrencyViewModel(
     private var ratesService: RatesService, private var coroutineScope: CoroutineScope
 ) : ViewModel(), KoinComponent {
     var allRatesServiceLiveData: MutableLiveData<RatesResponse> = MutableLiveData()
+
     var errorMutableLiveData: MutableLiveData<Boolean> = MutableLiveData()
+
     var allRatesMappedLiveData: LiveData<RatesMappedModel> =
         Transformations.switchMap(allRatesServiceLiveData) {
             ratesService.convertHashmapToList(it)
         }
 
-    init {
-    }
-
     fun getAllRates() {
-
         coroutineScope.launch {
             while (true) {
                 runCatching {
